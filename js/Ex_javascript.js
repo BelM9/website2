c983753573242be3b31ae3814018423637c4b2ca
BelM9 <bellahirechmoez@gmail.com>
2023-12-22 21:57:52 +0100

    update12

--- js/Ex_javascript.js
+++ js/Ex_javascript.js
@@ -1,8 +1,15 @@
 
 
-function Calculate_Factorial(int n) {
+int function Calculate_Factorial(int n) {
 
     for (let i = 1; i <= n; i++) {
        n*=n+1;  
        return(n);
    }
+
+
+   <p>Please enter an inetger to calculate its factoriel.</p>
+
+</body>/button onclick="Calculate_Factorial(j)">Clic here</button>
+
+<p id="Text-Res"></p>
